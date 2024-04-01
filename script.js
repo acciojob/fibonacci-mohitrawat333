3dc95724d37d41375c8cc5254180f3f8120a53f6
mohitrawat333 <151385966+mohitrawat333@users.noreply.github.com>
2024-04-01 22:24:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function fibonacci(num) {
 // your code here
+	function fibonacci(num) {
+    if (num === 0) return 0;
+    if (num === 1) return 0;
+    if (num === 2) return 1;
+    
+    let prevPrev = 0;
+    let prev = 1;
+    let current = 1;
+    
+    for (let i = 3; i <= num; i++) {
+        current = prevPrev + prev;
+        prevPrev = prev;
+        prev = current;
+    }
+    
+    return prevPrev;
 }
 
-module.exports = fibonacci;
+// Test cases
+console.log(fibonacci(1)); // Output: 0
+console.log(fibonacci(5)); // Output: 3
+	
+}
+
